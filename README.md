0c2d12f6ade1333c998275980d9e033759f45178
form43 <form43github@gmail.com>
2021-04-26 18:35:12 -0500

    Cambio

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rickmrpoojs.github.io
-Apuntes de Programación Orientada a Objetos.
+Apuntes de Programación Orientada a Objetos con JavaScript.
 
 https://rickmrpoojs.github.io
--- lib/css/estilos.css
+++ lib/css/estilos.css
@@ -1,4 +1,4 @@
-/* This work by Gilberto Pacheco Gallegos is licensed under the Creative Commons
+/* This work by Ricardo Armando Machorro Reyes is licensed under the Creative Commons
  * Atribución 4.0 Internacional License. To view a copy of this license, visit
  * http://creativecommons.org/licenses/by/4.0/. */
 
